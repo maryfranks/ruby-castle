7bffc21de6a38446c7cf6bbaac754efbfe410e0d
Mary Franks <maryfranks@gmail.com>
2018-07-09 19:38:49 -0400

    give the player a pack

--- player.rb
+++ player.rb
@@ -1,15 +1,17 @@
 class Player
-  # player info goes here
 
   INITIAL_HEALTH = 20
+  PACK_SIZE = 5
 
-  attr_accessor :name, :health, :max_health, :level
+  attr_accessor :name, :health, :max_health, :level, :pack, :pack_size
 
   def initialize(name)
     @name = name
     @health = INITIAL_HEALTH
     @max_health = INITIAL_HEALTH
     @level = 0
+    @pack = []
+    @pack_size = PACK_SIZE
   end
 
 end
