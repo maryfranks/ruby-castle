eee5ec9e117e0a236c7f479921fd2d5ab343fe96
Mary Franks <maryfranks@gmail.com>
2018-07-10 13:20:13 -0400

    add broom attributes and accessor methods

--- broom.rb
+++ broom.rb
@@ -1,3 +1,18 @@
+require_relative 'weapon.rb'
+
 class Broom < Weapon
+
+  attr_accessor :name, :strength, :sturdy, :description
+
+  def initialize
+
+    super
+
+    @name = "Broom"
+    @strength = 1
+    @sturdy = false
+    @description = "A broom, better for sweeping than defending yourself but hey, it's better than nothing."
+
+  end
 
 end
