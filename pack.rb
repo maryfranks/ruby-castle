7cd2a1de5aabb95fdf93e771483937efeacab346
Mary Franks <maryfranks@gmail.com>
2018-07-10 18:35:43 -0400

    use pack class to store items

--- pack.rb
+++ pack.rb
@@ -1,3 +1,14 @@
-class Pack 
+class Pack
+
+  attr_accessor :pack_size, :items 
+
+  PACK_SIZE = 5
+
+  def initialize
+
+    @pack_size = PACK_SIZE
+    @items = []
+
+  end
 
 end
--- player.rb
+++ player.rb
@@ -1,7 +1,8 @@
+require_relative 'pack.rb'
+
 class Player
 
   INITIAL_HEALTH = 20
-  PACK_SIZE = 5
 
   attr_accessor :name, :health, :max_health, :level, :pack, :pack_size
 
@@ -10,8 +11,6 @@ class Player
     @health = INITIAL_HEALTH
     @max_health = INITIAL_HEALTH
     @level = 0
-    @pack = []
-    @pack_size = PACK_SIZE
   end
 
 end
