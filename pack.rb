32cd6ebbd2b5d07c41fe6f837868074c5373dbb1
Mary Franks <maryfranks@gmail.com>
2018-07-10 18:43:39 -0400

    add methods to add and remove things from pack

--- pack.rb
+++ pack.rb
@@ -1,6 +1,6 @@
 class Pack
 
-  attr_accessor :pack_size, :items 
+  attr_accessor :pack_size, :items
 
   PACK_SIZE = 5
 
@@ -9,6 +9,14 @@ class Pack
     @pack_size = PACK_SIZE
     @items = []
 
+  end
+
+  def add_item(item)
+    @items.push(item)
+  end
+
+  def remove_item(item)
+    @items.delete(item)
   end
 
 end
