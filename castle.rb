649501d8adae59d979122c6c6243563545879aff
Mary Franks <maryfranks@gmail.com>
2018-09-16 20:14:00 -0400

    move entryway scenario to own method

--- castle.rb
+++ castle.rb
@@ -22,18 +22,9 @@ class Castle
   end
 
   def main
-    puts "As soon as you put the pen down you swear you hear a voice, it sounds like it's saying 'murder', but murder what? You take a step closer to the sound and you hear clearly - 'run #{@player.name}, before it's too late!' Something about the voice convinces you it isn't a haunted house after all. You turn to leave but the door is closed. It won't open, there isn't even a doorknob. What kind of asshole ghost warns you to leave after it's too late? Maybe it even closed the door on you. At any rate, you appear to be trapped, you might as well look around right? (yes/no)"
-    answer = gets.chomp
-    if answer == "yes"
-      puts "looking around is usually a good idea."
-      look_around("entryway")
-    elsif answer == "no"
-      puts "You die a violent death from something you didn't see in a dark corner somewhere. You didn't make an effort to look around, the castle didn't make an effort to give you an interesting death. No one finds your body. No one is even looking for you. Everyone forgets you."
-      @alive = false
-    else
-      puts "Your lack of basic comprehension makes me dubious that you'll survive the adventure. Luckily there's nothing in the room to kill you - you'd already be dead considering that you went staight to the guest book to sign in. Even the slowest of deaths would have had time to find you. You're going to look around now, and you should always look around when you enter a new room."
-      look_around("entryway")
-    end
+    self.entryway
+    self.instructions
+    # main game loop
     while @alive == true
       puts "what do you want to do? stay or go home"
       choice = gets.chomp
@@ -56,16 +47,34 @@ class Castle
   def look_around(room_type) # jason thinks I should put this shit in player
     if room_type = "entryway"
       puts "Start by looking in your pack. You might have something good in there. There is #{@player.pack.items.count} items in your pack."
-
       puts "You find a broom."
       broom = Broom.new
       @player.pack.items.push(broom)
-      puts "there is #{@player.pack.items.count} in your pack"
+      puts "there is #{@player.pack.items.count} items in your pack"
     end
   end
 
   def main_menu
     puts "these are your options: look around, "
+  end
+
+  def instructions
+    puts "game instructions should go here and you should be able to get back to them from the main menu"
+  end
+
+  def entryway
+    puts "As soon as you put the pen down you swear you hear a voice, it sounds like it's saying 'murder', but murder what? You take a step closer to the sound and you hear clearly - 'run #{@player.name}, before it's too late!' Something about the voice convinces you it isn't a haunted house after all. You turn to leave but the door is closed. It won't open, there isn't even a doorknob. What kind of asshole ghost warns you to leave after it's too late? Maybe it even closed the door on you. At any rate, you appear to be trapped, you might as well look around right? (yes/no)"
+    answer = gets.chomp
+    if answer == "yes"
+      puts "looking around is usually a good idea."
+      look_around("entryway")
+    elsif answer == "no"
+      puts "You die a violent death from something you didn't see in a dark corner somewhere. You didn't make an effort to look around, the castle didn't make an effort to give you an interesting death. No one finds your body. No one is even looking for you. Everyone forgets you."
+      @alive = false
+    else
+      puts "Your lack of basic comprehension makes me dubious that you'll survive the adventure. Luckily there's nothing in the room to kill you - you'd already be dead considering that you went staight to the guest book to sign in. Even the slowest of deaths would have had time to find you. You're going to look around now, and you should always look around when you enter a new room."
+      look_around("entryway")
+    end
   end
 
 end
