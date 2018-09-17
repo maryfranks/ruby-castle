a2572f44ec6d231ffea9e2382d45762ea71a1144
Mary Franks <maryfranks@gmail.com>
2018-09-17 09:15:52 -0400

    move entryway method and update look_around to describe broom

--- castle.rb
+++ castle.rb
@@ -43,14 +43,30 @@ class Castle
     end
   end
 
+  def entryway
+    puts "As soon as you put the pen down you swear you hear a voice, it sounds like it's saying 'murder', but murder what? You take a step closer to the sound and you hear clearly - 'run #{@player.name}, before it's too late!' Something about the voice convinces you it isn't a haunted house after all. You turn to leave but the door is closed. It won't open, there isn't even a doorknob. What kind of asshole ghost warns you to leave after it's too late? Maybe it even closed the door on you. At any rate, you appear to be trapped, you might as well look around right? (yes/no)"
+    answer = gets.chomp
+    if answer == "yes"
+      puts "looking around is usually a good idea. Start by looking in your pack. You might have something good in there."
+      @player.examine_pack
+      look_around("entryway")
+    elsif answer == "no"
+      puts "You die a violent death from something you didn't see in a dark corner somewhere. You didn't make an effort to look around, the castle didn't make an effort to give you an interesting death. No one finds your body. No one is even looking for you. Everyone forgets you."
+      @alive = false
+    else
+      puts "Your lack of basic comprehension makes me dubious that you'll survive the adventure. Luckily there's nothing in the room to kill you - you'd already be dead considering that you went staight to the guest book to sign in. Even the slowest of deaths would have had time to find you. You're going to look around now, and you should always look around when you enter a new room."
+      look_around("entryway")
+    end
+  end
+
   def look_around(room_type) # jason thinks I should put this shit in player
     if room_type == "entryway"
-      puts "Start by looking in your pack. You might have something good in there."
-      @player.examine_pack
-      puts "You find a broom."
+      puts "you're in the entryway"
       broom = Broom.new
+      puts "Over in the corner you see a broom! Hey this could be good!"
+      broom.describe_item
+      puts "Maybe you should keep it just in case."
       @player.pack.add_item(broom)
-      @player.examine_pack
     end
   end
 
@@ -62,19 +78,5 @@ class Castle
     puts "game instructions should go here and you should be able to get back to them from the main menu"
   end
 
-  def entryway
-    puts "As soon as you put the pen down you swear you hear a voice, it sounds like it's saying 'murder', but murder what? You take a step closer to the sound and you hear clearly - 'run #{@player.name}, before it's too late!' Something about the voice convinces you it isn't a haunted house after all. You turn to leave but the door is closed. It won't open, there isn't even a doorknob. What kind of asshole ghost warns you to leave after it's too late? Maybe it even closed the door on you. At any rate, you appear to be trapped, you might as well look around right? (yes/no)"
-    answer = gets.chomp
-    if answer == "yes"
-      puts "looking around is usually a good idea."
-      look_around("entryway")
-    elsif answer == "no"
-      puts "You die a violent death from something you didn't see in a dark corner somewhere. You didn't make an effort to look around, the castle didn't make an effort to give you an interesting death. No one finds your body. No one is even looking for you. Everyone forgets you."
-      @alive = false
-    else
-      puts "Your lack of basic comprehension makes me dubious that you'll survive the adventure. Luckily there's nothing in the room to kill you - you'd already be dead considering that you went staight to the guest book to sign in. Even the slowest of deaths would have had time to find you. You're going to look around now, and you should always look around when you enter a new room."
-      look_around("entryway")
-    end
-  end
 
 end
