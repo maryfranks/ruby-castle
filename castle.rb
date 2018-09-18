90086411a3a22827381320905f40e725cd8096b3
Mary Franks <maryfranks@gmail.com>
2018-09-18 12:37:25 -0400

    refactor castle to instantiate entryway as a room

--- castle.rb
+++ castle.rb
@@ -2,6 +2,7 @@ require_relative 'player.rb'
 require_relative 'pack.rb'
 require_relative 'broom.rb'
 require_relative 'snack.rb'
+require_relative 'room.rb'
 
 class Castle
   # where the main game lives
@@ -36,11 +37,15 @@ class Castle
   end
 
   def explore_entryway
+    entryway = Room.new(1)
+    entryway.description = "you're in the entryway"
+    # build the entryway bit by bit here
     puts "As soon as you put the pen down you swear you hear a voice, it sounds like it's saying 'murder', but murder what? You take a step closer to the sound and you hear clearly - 'run #{@player.name}, before it's too late!' Something about the voice convinces you it isn't a haunted house after all. You turn to leave but the door is closed. It won't open, there isn't even a doorknob. What kind of asshole ghost warns you to leave after it's too late? Maybe it even closed the door on you. At any rate, you appear to be trapped, you might as well look around right? (yes/no)"
     answer = gets.chomp
     if answer == "yes"
       puts "looking around is usually a good idea. Start by looking in your pack. You might have something good in there."
       @player.examine_pack
+      puts entryway.description
       look_around("entryway")
     elsif answer == "no"
       puts "You die a violent death from something you didn't see in a dark corner somewhere. You didn't make an effort to look around, the castle didn't make an effort to give you an interesting death. No one finds your body. No one is even looking for you. Everyone forgets you."
@@ -51,14 +56,14 @@ class Castle
       @player.examine_pack
     end
     puts "It's always a good idea to keep looking around to make sure you've found everything there is to find, but hey, I can't make you do anything. You have endless amounts of options, being trapped in a creepy castle. If you want to look around again, type 'look around'. If you want to see what other options are available, type 'main menu'."
-    while @player.level == 0
+    while @player.level == 0 && @alive == true 
       risk_your_life
-    end 
+    end
   end
 
   def look_around(room_type) # jason thinks I should put this shit in player
     if room_type == "entryway"
-      puts "you're in the entryway"
+      # puts "you're in the entryway"
       broom = Broom.new
       puts "Over in the corner you see a broom! Hey this could be good!"
       broom.describe_item
