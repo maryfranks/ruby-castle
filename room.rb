67637f227590379aed149c96716ae7531cca512f
Mary Franks <maryfranks@gmail.com>
2018-09-18 13:07:00 -0400

    move look around to room.rb and convert to methods that fill room attribute arrays

--- room.rb
+++ room.rb
@@ -1,35 +1,37 @@
 class Room
 
-  attr_reader :size, :things, :enemies, :dangers, :clues
+  attr_reader :size, :objects, :enemies, :dangers, :clues
   attr_accessor :description
 
   def initialize(size)
     @size = size
-    @things = []
-    @enemies = []
-    @dangers = []
-    @clues = []
+    @objects = [] # anything that can go in your pack
+    @enemies = [] # enemies can attack you before you look around sometimes, plus you fight them
+    @dangers = [] # dangers you usually blunder into and they can't be fought
+    @clues = [] # aren't like other items, they help you figure out how to get out of the castle and are added to the instructions
     @description = ""
   end
 
-  def add_things
-
+  def add_objects(object)
+    @objects.push(object)
   end
 
-  def add_enemies
-
+  def add_enemies(enemy)
+    @enemies.push(enemy)
   end
 
-  def add_dangers
-
+  def add_dangers(danger)
+    @dangers.push(danger)
   end
 
-  def add_clues
-
+  def add_clues(clue)
+    @clues.push(clue)
   end
 
-  def add_clues
-
+  def look_around
+      puts "Over in the corner you see a #{@objects.first.name}! Hey this could be good!"
+      @objects.first.name
+      puts "Maybe you should keep it just in case."
   end
 
 end
